c48aa3e58505649e715cd2941e937f63b5dc5ee7
dominic-wassef <domwassef@gmail.com>
2023-02-20 21:28:09 -0500

    adding client grpc

--- client/main.go
+++ client/main.go
@@ -3,22 +3,59 @@ package main
 import (
 	"context"
 	"log"
-	"net"
+	"time"
 
 	pb "dominic.com/gopher-grpc/proto"
-	"github.com/google/uuid"
+
 	"google.golang.org/grpc"
 )
 
 const (
 	// Port for gRPC server to listen to
-	PORT = ":50051"
+	ADDRESS = "localhost:50051"
 )
 
-type TodoServer struct {
-	pb.UnimplementedTodoServiceServer
+type TodoTask struct {
+	Name        string
+	Description string
+	Done        bool
 }
 
-func (s *TodoServer) CreateTodo(ctx context.Context, in *pb.NewTodo) (*pb.Todo, error) {
-	log.Printf("Recieved: %v", in.GetName())
+func main() {
+	conn, err := grpc.Dial(ADDRESS, grpc.WithInsecure(), grpc.WithBlock())
+
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+
+	defer conn.Close()
+
+	c := pb.NewTodoServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+
+	defer cancel()
+
+	todos := []TodoTask{
+		{Name: "Code review", Description: "Review a new code feature", Done: false},
+		{Name: "Make youtube view", Description: "Start go for GRPC", Done: false},
+		{Name: "Go to gym", Description: "Leg day", Done: false},
+		{Name: "Buy grocerties", Description: "Buy onion magos and bananas", Done: false},
+		{Name: "Meet with mentor", Description: "Discuss blockers with my project", Done: false},
+	}
+
+	for _, todo := range todos {
+		res, err := c.CreateTodo(ctx, &pb.NewTodo{Name: todo.Name, Description: todo.Description, Done: todo.Done})
+
+		if err != nil {
+			log.Fatalf("could not create user: %v", err)
+		}
+
+		log.Printf(`
+			ID : %s
+			Name : %s
+			Description : %s
+			Done : %v,
+		`, res.GetId(), res.GetName(), res.GetDescription(), res.GetDone())
+	}
 }
